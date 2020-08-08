dd34111fc53f08f08265515c18c8edb5e573c6dc
doyeon211 <69374408+doyeon211@users.noreply.github.com>
2020-08-08 13:33:13 +0900

    점심 전
    업데이트

---
+++ test.js
@@ -0,0 +1,26 @@
+//console.log("김")
+//alert("1")
+//let n = prompt("숫자 입력")
+//n = Number(n) //문자열을 숫자로 바꿔ㅓ준다
+//console.log(Number(n) + 10)
+//console.log(n + 30)
+//console.log(prompt("입력"))
+
+//1.
+//let n = prompt("숫자 입력")
+//console.log(Number(n) + 10)
+
+//2.
+//n = Number
+//console.log(n + 10)
+
+
+// let n = prompt("숫자 입력")
+// let a = prompt("숫자 입력")
+// let b = prompt("숫자 입력")
+// n = Number(n)
+// a = Number(a)
+// b = Number(b)
+// console.log(n + a + b)
+
+document.getElementById("first").innerHTML = document.getElementById("first").innerHTML + prompt()
