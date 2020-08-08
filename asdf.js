bfb776fc1608f5317457bf2199325c67dc6b33cf
doyeon211 <69374408+doyeon211@users.noreply.github.com>
2020-08-08 17:40:18 +0900

    최종

---
+++ asdf.js
@@ -0,0 +1,14 @@
+// 변수 예제
+// var let const의 차이
+// 재선언과 재할당
+
+// var 예제
+
+const a = 
+const x = 10
+const y = 20
+
+//a + 10
+//a + 20
+
+console.log
--- test.js
+++ test.js
@@ -6,14 +6,6 @@
 //console.log(n + 30)
 //console.log(prompt("입력"))
 
-//1.
-//let n = prompt("숫자 입력")
-//console.log(Number(n) + 10)
-
-//2.
-//n = Number
-//console.log(n + 10)
-
 
 // let n = prompt("숫자 입력")
 // let a = prompt("숫자 입력")
@@ -23,4 +15,101 @@
 // b = Number(b)
 // console.log(n + a + b)
 
-document.getElementById("first").innerHTML = document.getElementById("first").innerHTML + prompt()
+
+//document.getElementById("first").innerHTML = document.getElementById("first").innerHTML + prompt()
+
+
+//let a = prompt()
+//const x = 10
+//const y = 20
+
+//a = Number(a)
+
+//a + 10
+//a + 20
+
+//console.log(a + x)
+//console.log(a + y)
+
+
+// let result
+// let first_num = Number(prompt())
+// let second_num = Number(prompt())
+// let third_num = Number(prompt())
+// result = first_num + second_num + third_num
+// console.log(result)
+
+
+// let a = prompt()
+// let b = prompt()
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(a + b)
+// console.log(typeof (a + b))
+
+
+// var = 변수 
+// let = 변수 
+// const = 변수 절대값
+// prompt = 경고창 입력폼
+
+// var a = 1
+// var b = 2
+// console.log(1 > 2)
+// console.log(1 >= 2)
+// console.log(1 < 2)
+// console.log(1 <= 2)
+// console.log(a+=b)
+
+
+// let a = Number(prompt())
+// if (a>=90) {
+// 	console.log("통과")
+// }
+// else if (a>=70) {
+// 	console.log("보류")
+// }
+// else if (a>=40) {
+// 	console.log("재시험")
+// }
+// else {console.log("부모님 소환")}
+
+
+// let n = prompt()
+// switch (n) {
+// 	case 'a':
+// 	console.log("a")
+// 	break
+// 	case 'b':
+// 	console.log("b")
+// 	break
+// 	case 'c':
+// 	console.log("c")
+// 	break
+// 	case 'd':
+// 	console.log("d")
+// 	break
+// 	default:
+// 	console.log("입력이 잘못되었습니다.")
+// }
+
+// let n = prompt()
+// if (n == 'a') {
+// 	console.log("a")
+// }
+// else if (n == 'b') {
+// 	console.log("b")
+// }
+// else if (n == 'c') {
+// 	console.log("c")
+// }
+// else if (n == 'd') {
+// 	console.log("d")
+// }
+// else {console.log("입력이 잘못되었습니다.")}
+
+
+// for (var n=0; n<10; n++){
+// 	console.log('김도연')
+// 	console.log(n+1)
+// }
