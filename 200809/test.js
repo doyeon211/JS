24bd96bcd2ba1993274717a7c21042f032e87704
doyeon211 <69374408+doyeon211@users.noreply.github.com>
2020-08-09 11:03:56 +0900

    200809-11:00
    연습-1

---
+++ 200809/test.js
@@ -0,0 +1,51 @@
+let n = Number(prompt())
+if (n>=90) {
+    console.log("통과")
+}
+else if (n>=70) {
+    console.log("보류")
+}
+else if (n>=40) {
+    console.log("재시험")
+}
+else (console.log("부모님 소환"))
+
+
+let n = prompt()
+switch(n){
+    case 'a':
+        console.log("a")
+        break
+    case 'b':
+        console.log("b")
+        break
+    case 'c':
+        console.log("c")
+        break
+    case 'd':
+        console.log("d")
+        break
+    default:
+        console.log("입력이 잘못되었습니다.")
+}
+
+
+let n = prompt()
+if (n == 'a') {
+    console.log("a")
+}
+else if (n == 'b') {
+    console.log("b")
+}
+else if (n == 'c') {
+    console.log("c")
+}
+else if (n == 'd') {
+    console.log("d")
+}
+else (console.log("입력이 잘못되었습니다."))
+
+
+for (let n=0; n<10; n++) {
+    console.log("김도연")
+}
